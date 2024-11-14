9706cc660ec3a7ef7c6548e5e985c912a3465fd9
Alizamankhan786 <az030366@gmail.com>
2024-11-14 04:29:03 -0800

    apis done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // const express = require('express')
+<<<<<<< HEAD
 import express from "express";
+=======
+import express, { json } from "express";
+>>>>>>> 3f0913d (apis done)
 const app = express()
 const port = 3000
 
@@ -7,6 +11,56 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+<<<<<<< HEAD
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+=======
+
+
+//Add Users
+
+app.post("/users" , (req , res) => {
+    const {name} = req.body;
+    if(!name) {
+        res.json({
+            message: "Name is Required"
+        });
+        return;
+    };
+    
+    users.push({
+        id: Date.now(),
+        name,
+    });
+    res.send({
+        message: "Title is Required",
+        data: users,
+    });
+});
+
+
+// Single User
+
+app.get("/users" , (req , res) => {
+    const {id} = req.params
+
+    const index = users.findIndex((item) => item.id === +id);
+
+    if(index == -1){
+        req.status(404).json({
+            message: "User not Found"
+        });
+        return;
+    }
+
+    req.status(200).json({
+        data: users[index],
+    });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+>>>>>>> 3f0913d (apis done)
